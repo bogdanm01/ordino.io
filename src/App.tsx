65efd61b568cb994f91762224082a658d65446ee
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-02-15 12:26:49 +0100

    Create Vite project and add dependencies

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import "@mantine/core/styles.css";
+
+import { createTheme, MantineProvider } from "@mantine/core";
+import AppLayout from "./Components/AppLayout/AppLayout";
+
+const theme = createTheme({
+  // fontFamily: "Roboto, sans-serif",
+});
+
+function App() {
+  return <MantineProvider theme={theme}></MantineProvider>;
+}
+
+export default App;
